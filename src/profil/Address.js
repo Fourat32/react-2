00e08ed7e9ad5fe2d100fbdd4b434f1a3ac5bc07
Youssef fourat <yousseffourat32@gmail.com>
2022-12-20 15:46:17 +0100

    first commit

---
+++ src/profil/Address.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Address = () => {
+  return (
+    <div>Address : Monastir</div>
+  )
+}
+
+export default Address
---
+++ src/profil/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import img from './photos.jpg'
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src={img}  width={"250px"} height={'400px'}></img>
+                      </div>)
+}
+
+export default ProfilePhoto
